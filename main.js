28615bd9404a37ddf5134a058b883627586469f4
Info WhitePearlLabs <05200233.jnec@rub.edu.bt>
2023-05-07 23:55:51 +0600

    added resizes

--- main.js
+++ main.js
@@ -36,3 +36,26 @@ const canvas = document.querySelector('.webgl');
 const renderer = new THREE.WebGLRenderer({ canvas })
 renderer.setSize(sizes.width, sizes.width)
 renderer.render(scene, camera)
+
+
+
+// RESIZE
+window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+    renderer.setSize(sizes.width, sizes.height)
+
+})
+const loop = () => {
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(loop)
+
+
+}
+
+loop()
