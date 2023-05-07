7a6003d4cb30a7c0d1cb0cca7a10cc448f683781
Info WhitePearlLabs <05200233.jnec@rub.edu.bt>
2023-05-08 00:40:35 +0600

    added animation

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import "./style.css"
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { gsap } from 'gsap';
 
 // Scene
 const scene = new THREE.Scene();
@@ -68,3 +69,31 @@ const loop = () => {
 }
 
 loop()
+
+// Timeline magic
+const tl = gsap.timeline({ defaults: { duration: 1 } })
+// scaling all the axis at the same time
+tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 })
+tl.fromTo("nav", { y: "-100%" }, { y: "0%" })
+tl.fromTo(".title", { opacity: 0 }, { opacity: 1 })
+
+
+// Mouse Animation Color
+let mouseDown = false
+let rgb = []
+window.addEventListener("mousedown", () => (mouseDown = true))
+window.addEventListener("mouseup", () => (mouseDown = false))
+
+window.addEventListener("mousemove", (e) => {
+    if (mouseDown) {
+        rgb = [Math.round((e.pageX / sizes.width) * 255), Math.round((e.pageY / sizes.height) * 255), 150]
+
+        // Let's animate
+        let newColor = new THREE.Color(`rgb(${rgb.join(",")})`)
+        gsap.to(mesh.material.color, {
+            r: newColor.r,
+            g: newColor.g,
+            b: newColor.b
+        })
+    }
+})
